c565e3de9b4e5377d8181f7745a9fec644355e89
Nate <nate.crow76@gmail.com>
2017-04-28 22:58:12 -0400

    more profile changes
    - changed sex to gender
    - added signals so Profile model will be auto created/updated when User
    instances are created/updated

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -9,11 +11,19 @@ class Profile(models.Model):
     birth_date = models.DateField(blank=True, null=True)
     location = models.CharField(max_length=30, blank=True)
     
-    class Sexes(Enum):
-        male = 0
-        female = 1
-        other = 2
-    sex = models.SmallIntegerField(choices=[(s.value, s.name) for s in Sexes], blank=True, null=True)
+    class Genders(Enum):
+        male = 'm'
+        female = 'f'
+    gender = models.CharField(max_length=1, choices=[(s.value, s.name) for s in Genders], blank=True)
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+    
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
     
     def __str__(self):
         return self.name or str(self.user)
