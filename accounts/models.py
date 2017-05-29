5338580600bc82c9a9d6bb09206dfecf9f30fe4c
natecrow <nate.crow76@gmail.com>
2017-05-29 00:30:39 -0400

    Displayed more info in profile page

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,6 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(blank=True, null=True)
     location = models.CharField(max_length=30, blank=True)
     
@@ -16,24 +15,22 @@ class Profile(models.Model):
         male = 'm'
         female = 'f'
     gender = models.CharField(max_length=1, choices=[(s.value, s.name) for s in Genders], blank=True)
-    
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        """
-        When a User is created, create a corresponding Profile for it.
-        """
-        
-        if created:
-            Profile.objects.create(user=instance)
-    
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        """
-        When a User is saved, save its corresponding Profile too.
-        """
-        
-        instance.profile.save()
-    
+
     def __str__(self):
         return self.name or str(self.user)
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    When a User is created, create a corresponding Profile for it.
+    """
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """
+    When a User is saved, save its corresponding Profile too.
+    """
+    instance.profile.save()
     
