859432e4aa6f66241fbfcb6ef7eda62f1d695009
natecrow <nate.crow76@gmail.com>
2017-06-07 00:08:29 -0400

    Tested that test user was created successfully

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -8,23 +8,43 @@ TEST_EMAIL = 'test123@test.com'
 TEST_USERNAME = 'test123'
 TEST_PASSWORD = 'test123'
 TEST_BIRTH_DATE = '1990-01-01'
+TEST_GENDER = 'm'
 TEST_LOCATION = 'somewhere'
  
 class LoginTestCase(TestCase):
-    def tearDown(self):
-        User.objects.get(username=TEST_USERNAME).delete()
-    
     def setUp(self):
-        # create test user
-        user = User.objects.create_user(
+        # create test test_user
+        self.test_user = User.objects.create_user(
             first_name=TEST_FIRST_NAME,
             last_name=TEST_LAST_NAME,
             email=TEST_EMAIL,
             username=TEST_USERNAME,
             password=TEST_PASSWORD,
-            #birth_date=TEST_BIRTH_DATE,
-            #location=TEST_LOCATION,
         )
-        user.is_active = True
-        user.save()
+        self.test_user.profile.birth_date = TEST_BIRTH_DATE
+        self.test_user.profile.location = TEST_LOCATION
+        self.test_user.profile.gender = TEST_GENDER
+        self.test_user.is_active = True
+        self.test_user.save()
+
+    def tearDown(self):
+        User.objects.get(username=TEST_USERNAME).delete()
+    
+    def test_user_first_name_set_correctly(self):
+        self.assertEqual(self.test_user.first_name, TEST_FIRST_NAME)
+        
+    def test_user_first_name_set_correctly(self):
+        self.assertEqual(self.test_user.last_name, TEST_LAST_NAME)
+        
+    def test_user_email_set_correctly(self):
+        self.assertEqual(self.test_user.email, TEST_EMAIL)
+        
+    def test_user_birth_date_set_correctly(self):
+        self.assertEqual(self.test_user.profile.birth_date, TEST_BIRTH_DATE)
+        
+    def test_user_gender_set_correctly(self):
+        self.assertEqual(self.test_user.profile.gender, TEST_GENDER)
+        
+    def test_user_location_set_correctly(self):
+        self.assertEqual(self.test_user.profile.location, TEST_LOCATION)        
         
