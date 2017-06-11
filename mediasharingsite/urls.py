e34dd1e4549890e7d69d4918c74d7f2efc9f28fd
natecrow <nate.crow76@gmail.com>
2017-06-10 23:57:01 -0400

    Added home page (placeholder for now)

--- mediasharingsite/urls.py
+++ mediasharingsite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='homepage.html'), name="homepage"),
+
     url(r'^admin/', admin.site.urls),
 
     url(r'^accounts/', include('accounts.urls')),
