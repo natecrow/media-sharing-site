6106220ee50b4fac5952f4e8e2d5c066542385b1
natecrow <nate.crow76@gmail.com>
2017-06-13 23:33:37 -0400

    Added logout link to profile page for now

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,6 @@ app_name = 'accounts'
 urlpatterns = [
     url(r'^profile$', views.profile, name='profile'),
     url(r'^login$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^logout$', auth_views.logout, {'template_name': 'accounts/logged_out.html', 'next_page': 'login'}, name='logout'),
+    url(r'^logout$', auth_views.logout, {'template_name': 'accounts/logged_out.html'}, name='logout'),
     url(r'^signup$', views.signup, name='signup'),
 ]
