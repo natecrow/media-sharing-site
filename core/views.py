f968d45588c544ca7360278d7f6bb23ce6eaa563
natecrow <nate.crow76@gmail.com>
2017-07-01 00:07:31 -0400

    Removed simple upload

--- core/views.py
+++ core/views.py
@@ -7,24 +7,17 @@ from core.models import Document
 
 
 def model_form_upload(request):
+    # if request is a POST, then process the form data
+    # (i.e. user filled out the form and is sending data back)
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('homepage')
+    # otherwise create the form
+    # (i.e. user has not filled out the form)
     else:
         form = DocumentForm()
     return render(request, 'core/model_form_upload.html', {
         'form': form
     })
-
-def simple_upload(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request, 'core/simple_upload.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
-    return render(request, 'core/simple_upload.html')
