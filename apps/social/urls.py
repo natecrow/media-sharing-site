e663a1e50ef64e7c71c3942e1f8b539db09faebb
natecrow <nate.crow76@gmail.com>
2017-08-05 16:31:38 -0400

    Link to user profiles from directory

--- apps/social/urls.py
+++ apps/social/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'social'
 urlpatterns = [
     url(r'^$', views.profiles_directory, name='profiles_directory'),
-    url(r'^(?P<username>[a-zA-Z0-9]+)$', views.profile_page, name='profile_page'),
+    url(r'^(?P<username>[a-zA-Z0-9]+)$',
+        views.profile_page, name='profile_page'),
 ]
