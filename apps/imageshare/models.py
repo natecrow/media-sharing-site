f5d7e90ac783576b1ec80d4765b0dd5b3821f966
natecrow <nate.crow76@gmail.com>
2017-10-16 23:17:27 -0400

    Installed django-tagulous and added tags field to Image model

--- apps/imageshare/models.py
+++ apps/imageshare/models.py
@@ -2,10 +2,17 @@ import os
 
 from django.contrib.auth.models import User
 from django.db import models
+from tagulous import models as tagulous_models
 
 
 def get_image_path(instance, filename):
     return os.path.join('user', str(instance.user.id), filename)
+
+
+class ImageTag(tagulous_models.TagModel):
+    class TagMeta:
+        force_lowercase = True
+        # autocomplete_view = 'tags_autocomplete'
 
 
 class MediaFile(models.Model):
@@ -20,6 +27,7 @@ class MediaFile(models.Model):
 
 class Image(MediaFile):
     image = models.ImageField(upload_to=get_image_path)
+    tags = tagulous_models.TagField(to=ImageTag)
 
     def __str__(self):
         return str(self.image.name)
--- mediasharingsite/settings.py
+++ mediasharingsite/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
     # external apps
     'sorl.thumbnail',
+    'tagulous',
 ]
 
 MIDDLEWARE = [
