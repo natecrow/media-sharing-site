9a57c7f9237aca7e086641e1d24441a7bd21f40f
natecrow <nate.crow76@gmail.com>
2017-12-16 17:28:12 -0500

    Added colors and categories to images

--- apps/imageshare/models.py
+++ apps/imageshare/models.py
@@ -20,6 +20,22 @@ class MediaFile(models.Model):
         User, on_delete=models.CASCADE, related_name='images')
     uploaded_date = models.DateTimeField(
         null=False, blank=False, auto_now_add=True)
+    CATEGORIES = [
+        ('ABSTRACT', 'abstract'),
+        ('ANIMALS', 'animals'),
+        ('ARCHITECTURE', 'architecture'),
+        ('FANTASY', 'fantasy'),
+        ('FOOD', 'food'),
+        ('HOLIDAYS', 'holidays'),
+        ('MISC', 'misc'),
+        ('MUSIC', 'music'),
+        ('NATURE', 'nature'),
+        ('SCI-FI', 'sci-fi'),
+        ('SPACE', 'space'),
+        ('SPORTS', 'sports'),
+        ('VEHICLES', 'vehicles'),
+    ]
+    category = models.CharField(max_length=20, choices=CATEGORIES, default='MISC')
 
     class Meta:
         abstract = True
@@ -28,6 +44,21 @@ class MediaFile(models.Model):
 class Image(MediaFile):
     image = models.ImageField(upload_to=get_image_path)
     tags = tagulous_models.TagField(to=ImageTag)
+    COLORS = [
+        ('RED', 'red'),
+        ('ORANGE', 'orange'),
+        ('YELLOW', 'yellow'),
+        ('GREEN', 'green'),
+        ('TEAL', 'teal'),
+        ('BLUE', 'blue'),
+        ('PURPLE', 'purple'),
+        ('PINK', 'pink'),
+        ('WHITE', 'white'),
+        ('GRAY', 'gray'),
+        ('BLACK', 'black'),
+        ('BROWN', 'brown'),
+    ]
+    color = models.CharField(max_length=6, choices=COLORS, blank=True)
 
     def __str__(self):
         return str(self.image.name)
--- apps/imageshare/urls.py
+++ apps/imageshare/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^upload-images$',
         views.ImageUploadView.as_view(), name='upload_images'),
     url(r'^images$', views.images, name='images'),
-    url(r'^images/(?P<image_id>[a-zA-Z0-9]+)$', views.view_image, name='view_image'),
+    url(r'^images/(?P<image_id>[a-zA-Z0-9]+)$',
+        views.view_image, name='view_image'),
 ]
